bd295225bc114184185f96f330af66857f11006e
user.email "ismailahmed.jsm@gmail.com <user.email : "ismailahmed.jsm@gmail.com>
2024-11-01 00:19:24 +0500

    created routes

--- app.js
+++ app.js
@@ -5,16 +5,20 @@ let port = 3000;
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const db = require("./config/mongodb-config")
+const ownersRouter = require('./routes/ownersRouter');
+const usersRouter = require("./routes/usersRouter"); 
+const productsRouter = require("./routes/productsRouter");
 
-const ownersRouter = require('./routes/ownersRouter')
-
+app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
-app.set('view engine', 'ejs');
 
-app.use('/owners', ownersRouter)
+
+app.use('/owners', ownersRouter);
+app.use('/users', usersRouter);
+app.use('/products', productsRouter);
 
 app.listen(port, (err) => {
     if(err)  console.log(err);
